2b70878088b8daef3d38e61b6821730b55a2d765
Oscar Mora <oscgmora@gmail.com>
2023-02-20 23:24:55 -0500

    Add Layout
    This commit adds all the layout needed for the landing page of the library when it is first brought up.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const addBook = document.getElementsByClassName("add-book").addEventListener("click", addBookInfo);
+const logIn = document.getElementsByClassName("log-in");
+
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
