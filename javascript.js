ffa0e4c0a36ed1e2ebea9bef211772b32819d7ac
Oscar Mora <oscgmora@gmail.com>
2023-02-25 23:58:01 -0500

    Add Book Addition Functionality for One Book
    This commit adds javascript for adding one book to the library. It displays title, author, and amount of pages accurately. It also adds a small bit of styling to the cards, such as their grid positions. It fixes as previous problem of the book cards going between the footer and main section. Now, they have been fixed to go into their own container, in the main section. The grid adjusts depending on the window size.

--- javascript.js
+++ javascript.js
@@ -1,14 +1,76 @@
+let overlay = document.getElementById("overlay");
+let myForm = document.getElementById("myForm");
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
 function openForm() {
-  document.getElementById("myForm").style.display = "block";
+  myForm.style.display = "block";
+  overlay.classList.add("active");
 }
 
 function closeForm() {
-  document.getElementById("myForm").style.display = "none";
+  myForm.style.display = "none";
+  overlay.classList.remove("active");
 }
 
 window.onclick = function (event) {
-  let modal = document.getElementById("myForm");
-  if (event.target == modal) {
+  if (event.target == overlay) {
     closeForm();
+  }
+}
+
+function addBookToLibrary() {
+  let title = document.getElementById("title");
+  let author = document.getElementById("author");
+  let pages = document.getElementById("pages");
+  let read = document.getElementById("read");
+  let book = new Book(title.value, author.value, pages.value, read.value);
+  myLibrary.push(book);
+  console.log(myLibrary);
+}
+
+function submitForm() {
+  event.preventDefault();
+  closeForm();
+  addBookToLibrary();
+  displayLibrary();
+  document.getElementById("form-container").reset();
+}
+
+function displayLibrary() {
+  for (let i = 0; i < myLibrary.length; i++) {
+    let newBookCard = document.createElement("div");
+    newBookCard.classList.add("newBookCard");
+    let newTitleCard = document.createElement("div");
+    newTitleCard.classList.add("newTitleCard");
+    let newAuthorCard = document.createElement("div");
+    newTitleCard.classList.add("newAuthorCard");
+    let newPagesCard = document.createElement("div");
+    newTitleCard.classList.add("newPagesCard");
+    let newReadCard = document.createElement("div");
+    newTitleCard.classList.add("newReadCard");
+    let newTitle = document.createTextNode(myLibrary[i].title);
+    let newAuthor = document.createTextNode(myLibrary[i].author);
+    let newPages = document.createTextNode(myLibrary[i].pages);
+    let newRead = document.createTextNode(myLibrary[i].read);
+    newTitleCard.appendChild(newTitle);
+    newAuthorCard.appendChild(newAuthor);
+    newPagesCard.appendChild(newPages);
+    newReadCard.appendChild(newRead);
+    newBookCard.appendChild(newTitleCard);
+    newBookCard.appendChild(newAuthorCard);
+    newBookCard.appendChild(newPagesCard);
+    newBookCard.appendChild(newReadCard);
+    let currentBookCard = document.getElementById("book-card-container");
+    currentBookCard.appendChild(newBookCard);
   }
 }
