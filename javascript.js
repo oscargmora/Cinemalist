0da87761e2b4e2f86c73e657bba49a2af44c5e56
Oscar Mora <oscgmora@gmail.com>
2023-03-01 23:10:06 -0500

    Add Checkbox Checker
    This commit adds a function to check if the checkbox was checked in the form. If so, it changes what the book card says. It also adds a button for removal of a book from the library.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let overlay = document.getElementById("overlay");
 let myForm = document.getElementById("myForm");
+let check = false;
 
 let myLibrary = [];
 
@@ -28,15 +29,27 @@ window.onclick = function (event) {
   }
 }
 
+function readCheckbox() {
+  for (i = 0; i <= myLibrary.length; ++i) {
+    if (read.checked === true) {
+      check = true;
+    } else {
+      check = false;
+    }
+  }
+}
+
 function addBookToLibrary() {
   let title = document.getElementById("title");
   let author = document.getElementById("author");
   let pages = document.getElementById("pages");
   let read = document.getElementById("read");
   let book = new Book(title.value, author.value, pages.value, read.checked);
+  readCheckbox();
   myLibrary.push(book);
   console.log(myLibrary);
 }
+
 
 const submitForm = (e) => {
   e.preventDefault();
@@ -56,53 +69,49 @@ function displayLibrary() {
     let newAuthorCard = document.createElement("p");
     let newPagesCard = document.createElement("p");
     let newReadCard = document.createElement("button");
+    let newRemoveCard = document.createElement("button");
     let newTitle = document.createTextNode(myLibrary[i].title);
     let newAuthor = document.createTextNode(myLibrary[i].author);
     let newPages = document.createTextNode(`${myLibrary[i].pages} pages`);
-    let newRead = document.createTextNode(myLibrary[i].read);
+    let newRemove = document.createTextNode("Remove");
+    if (check === true) {
+      newReadCard.textContent = "Read";
+      newReadCard.classList.add("newReadCardGreen");
+      newReadCard.classList.remove("NewReadCardRed")
+    } else {
+      newReadCard.textContent = "Not Read";
+      newReadCard.classList.add("newReadCardRed");
+      newReadCard.classList.remove("newReadCardGreen");
+    };
     newTitleCard.appendChild(newTitle);
     newAuthorCard.appendChild(newAuthor);
     newPagesCard.appendChild(newPages);
-    newReadCard.appendChild(newRead);
+    newRemoveCard.appendChild(newRemove);
     newBookCard.appendChild(newTitleCard);
     newBookCard.appendChild(newAuthorCard);
     newBookCard.appendChild(newPagesCard);
     newBookCard.appendChild(newReadCard);
+    newBookCard.appendChild(newRemoveCard);
     newTitleCard.classList.add("newTitleCard");
     newAuthorCard.classList.add("newAuthorCard");
     newPagesCard.classList.add("newPagesCard");
     newReadCard.classList.add("newReadCard");
+    newRemoveCard.classList.add("newRemoveCard");
     let currentBookCard = document.getElementById("book-card-container");
     currentBookCard.appendChild(newBookCard);
   }
 }
 
-/* Possible checkbox checkers
-function readCheckbox() {
-  read.addEventListener('change', (event) => {
-  if (event.currentTarget.checked) {
-    newReadCard.textContent = "Read";
-    newReadCard.classList.add("newReadCardGreen");
-    newReadCard.classList.remove("NewReadCardRed")
-    console.log("Checked");
-  } else {
+//$("#book-card-container").on("click", ".newReadCard", changeReadStatus());
+
+function changeReadStatus() {
+  if (check === true) {
     newReadCard.textContent = "Not Read";
     newReadCard.classList.add("newReadCardRed");
     newReadCard.classList.remove("newReadCardGreen");
-    console.log("Not Checked");
-  }
-})
-}
-
-if (myLibrary.find(e => e.read === false)) {
-  newReadCard.textContent = "Not Read";
-  newReadCard.classList.add("newReadCardRed");
-  newReadCard.classList.remove("newReadCardGreen");
-  console.log("Not Checked");
-  } else {
+  } else if (check === false) {
     newReadCard.textContent = "Read";
     newReadCard.classList.add("newReadCardGreen");
-    newReadCard.classList.remove("NewReadCardRed")
-    console.log("Checked");
+    newReadCard.classList.remove("NewReadCardRed");   
   }
-  */
+}
