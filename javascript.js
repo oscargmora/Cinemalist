d21b09b4868b601548607e883f77765eb51a6409
Oscar Mora <oscgmora@gmail.com>
2023-03-06 22:37:19 -0500

    Add Remove Button Functionality
    This commit adds a unique ID to each book card. This also makes the remove button work both with the user interface, as well as the array data.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,23 @@
 let overlay = document.getElementById("overlay");
 let myForm = document.getElementById("myForm");
-let check = false;
+let container = document.getElementById("book-card-container");
 
 let myLibrary = [];
 
+function resetBooksGrid() {
+  container.innerHTML = "";
+}
+
 class Book {
-  constructor(title, author, pages, read) {
+  constructor(title, author, pages, read, id) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-  }
-}
+    id = `${Date.now()}`;
+    this.id = id;
+    }
+ }
 
 function openForm() {
   myForm.style.display = "block";
@@ -29,23 +35,12 @@ window.onclick = function (event) {
   }
 }
 
-function readCheckbox() {
-  for (i = 0; i <= myLibrary.length; ++i) {
-    if (read.checked === true) {
-      check = true;
-    } else {
-      check = false;
-    }
-  }
-}
-
 function addBookToLibrary() {
   let title = document.getElementById("title");
   let author = document.getElementById("author");
   let pages = document.getElementById("pages");
   let read = document.getElementById("read");
-  let book = new Book(title.value, author.value, pages.value, read.checked);
-  readCheckbox();
+  book = new Book(title.value, author.value, pages.value, read.checked);
   myLibrary.push(book);
   console.log(myLibrary);
 }
@@ -62,56 +57,74 @@ const submitForm = (e) => {
 myForm.onsubmit = submitForm;
 
 function displayLibrary() {
+  resetBooksGrid();
   for (let i = 0; i < myLibrary.length; i++) {
     let newBookCard = document.createElement("div");
-    newBookCard.classList.add("newBookCard");
     let newTitleCard = document.createElement("h1");
     let newAuthorCard = document.createElement("p");
     let newPagesCard = document.createElement("p");
     let newReadCard = document.createElement("button");
     let newRemoveCard = document.createElement("button");
-    let newTitle = document.createTextNode(myLibrary[i].title);
-    let newAuthor = document.createTextNode(myLibrary[i].author);
-    let newPages = document.createTextNode(`${myLibrary[i].pages} pages`);
-    let newRemove = document.createTextNode("Remove");
-    if (check === true) {
+
+    if (read.checked === true) {
       newReadCard.textContent = "Read";
       newReadCard.classList.add("newReadCardGreen");
-      newReadCard.classList.remove("NewReadCardRed")
+      newReadCard.classList.remove("NewReadCardRed");
     } else {
       newReadCard.textContent = "Not Read";
       newReadCard.classList.add("newReadCardRed");
       newReadCard.classList.remove("newReadCardGreen");
     };
-    newTitleCard.appendChild(newTitle);
-    newAuthorCard.appendChild(newAuthor);
-    newPagesCard.appendChild(newPages);
-    newRemoveCard.appendChild(newRemove);
-    newBookCard.appendChild(newTitleCard);
-    newBookCard.appendChild(newAuthorCard);
-    newBookCard.appendChild(newPagesCard);
-    newBookCard.appendChild(newReadCard);
-    newBookCard.appendChild(newRemoveCard);
+
+    newBookCard.classList.add("newBookCard");
     newTitleCard.classList.add("newTitleCard");
     newAuthorCard.classList.add("newAuthorCard");
     newPagesCard.classList.add("newPagesCard");
     newReadCard.classList.add("newReadCard");
     newRemoveCard.classList.add("newRemoveCard");
-    let currentBookCard = document.getElementById("book-card-container");
-    currentBookCard.appendChild(newBookCard);
+    newRemoveCard.setAttribute("data-id", myLibrary[i].id);
+
+    newReadCard.addEventListener("click", changeReadStatus);
+    newRemoveCard.addEventListener("click", removeBook);
+
+    let newTitle = document.createTextNode(myLibrary[i].title);
+    let newAuthor = document.createTextNode(myLibrary[i].author);
+    let newPages = document.createTextNode(`${myLibrary[i].pages} pages`);
+    let newRemove = document.createTextNode("Remove");
+
+    newTitleCard.appendChild(newTitle);
+    newAuthorCard.appendChild(newAuthor);
+    newPagesCard.appendChild(newPages);
+    newRemoveCard.appendChild(newRemove);
+
+    newBookCard.appendChild(newTitleCard);
+    newBookCard.appendChild(newAuthorCard);
+    newBookCard.appendChild(newPagesCard);
+    newBookCard.appendChild(newReadCard);
+    newBookCard.appendChild(newRemoveCard);
+
+    container.appendChild(newBookCard);
+  };
+}
+
+function changeReadStatus(e) {
+  	if (e.target.innerText == 'Read') {
+      e.target.textContent = "Not Read";
+      e.target.classList.add("newReadCardRed");
+      e.target.classList.remove("newReadCardGreen");
+      book.read = false;
+      console.log(myLibrary);
+	  } else if (e.target.innerText == 'Not Read') {
+      e.target.textContent = "Read";
+      e.target.classList.add("newReadCardGreen");
+      e.target.classList.remove("newReadCardRed");
+      book.read = true;
+      console.log(myLibrary);
   }
 }
 
-//$("#book-card-container").on("click", ".newReadCard", changeReadStatus());
-
-function changeReadStatus() {
-  if (check === true) {
-    newReadCard.textContent = "Not Read";
-    newReadCard.classList.add("newReadCardRed");
-    newReadCard.classList.remove("newReadCardGreen");
-  } else if (check === false) {
-    newReadCard.textContent = "Read";
-    newReadCard.classList.add("newReadCardGreen");
-    newReadCard.classList.remove("NewReadCardRed");   
-  }
+function removeBook(e) {
+      let objectID = e.target.getAttribute("data-id");
+      myLibrary = myLibrary.filter((Book) => Book.id != objectID); //Ask ChatGPT what's going on here
+      displayLibrary();
 }
